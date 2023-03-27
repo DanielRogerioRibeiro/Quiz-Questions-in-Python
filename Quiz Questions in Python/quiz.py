e6fc17fc8e1d2ab2d5abcafe147fba07e531ab3a
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-03-26 22:27:25 -0300

    Exclusion of option for alternatives and inclusion of manual roof entry.

--- Quiz Questions in Python/quiz.py
+++ Quiz Questions in Python/quiz.py
@@ -1,35 +1,93 @@
 # Quiz sobre Python
 # Este quiz tem como objetivo apresentar uma série de perguntas sobre Python para que o usuário responda
+from tkinter import *
+import random
 
-print ("*****Seja Bem-Vindo ao Quiz Python*****")
+#Cores
+cor1 ='#dfe0e8' #gray
+cor2 ='#0dff00' #green
+cor3 ='#3852d1' #blue
+cor4 ='#050505' #black
 
+# Cria uma window
+window = Tk()
+window.title("QUESTÕES SOBRE PYTHON")
+window.geometry('600x350') 
+window.iconphoto(False, PhotoImage(file="Quiz Questions in Python/imagens/Python_logo.png"))
+window.resizable(width=False,height=False)
+
+img = PhotoImage(file="Quiz Questions in Python/imagens/Python_logo.png")
+
+Label.logo = Label(window, image=img, anchor='center').pack()
+
+# Cria uma label para exibibição
+title_1 = Label (window, width=50, text= '*****Seja Bem-Vindo ao Quiz Python*****', font=('Arial 15'), fg=cor2, bg='blue', pady=5)
+title_1.place(x=20, y=0)
+
+title2_question = Label(window, width=50, height=2 , text="", font=('Arial 15'), fg='black')
+title2_question.place(x=20, y=120)
+
+# Cria uma entrada de texto para o usuário digitar a resposta
+input_answer = Entry(window, font=('Arial 15'), bg=cor1, fg=cor4)
+input_answer.place (x=180, y=200)
 
 
 # Lista de perguntas e respostas
-questions = {
+questions_answer = {
     "Qual é o operador de atribuição em Python? ": "=",
     "Qual é o operador para concatenar duas strings em Python? ": "+",
+    "Como se chama o método que adiciona um elemento\nao final de uma lista em Python?": "append",
     "Qual é o resultado da expressão 2 + 2 * 3? ": "8",
-    "Qual é a função para imprimir algo na tela em Python? ": "print",
-    "Qual é o método para adicionar um elemento a uma lista em Python? ": "append",
+    "Qual é o nome da função para imprimir algo na\ntela em Python? ": "print" or "Print" or "PRINT",
     "Qual é a função usada para ler a entrada do usuário?": "input",
     "Qual é a sintaxe para definir uma função em Python?": "def nome_da_funcao():"
 }
 
-#Função para executar o Quiz
-def run_quiz(questions):
-    score = 0
- 
-#Inicio do quiz
-    for question, answer in questions.items():
-        user_answer = input(question)
-        if user_answer.lower() == answer.lower():
-            print("Resposta correta!")
-            score += 1
-        else:
-            print("Resposta incorreta.")
-        
-        print(f"Você acertou {score} de {len(questions)} perguntas.")
+# Lista com as perguntas do Quiz
+questions = list(questions_answer.keys())
 
-# Executa o quiz
-run_quiz(questions)
+# Função para verificar a resposta do usuário
+def confirm_answer():
+    answer_usuario = input_answer.get()
+    answer_correct = questions_answer[title2_question["text"]]
+    
+    if answer_usuario == answer_correct:
+        label_answer = Label(window, text="Resposta correta!", font=("Arial", 14), fg="green")
+        
+    else:
+        label_answer = Label(window, width=30, height=1, text="A Resposta Correta era: {}" .format(answer_correct), font=("Arial", 14), fg="red", bg=None)
+        
+    
+    label_answer.place (x=150, y=250)
+    next_question()
+    
+
+# Cria um botão para o usuário confirmar sua resposta
+button_confirm = Button(window, text="Next", font=("Arial", 14),  command=confirm_answer)
+button_confirm.place (x=480, y=300)
+
+# Função para escolher a próxima pergunta
+def next_question():
+    # Escolhe uma pergunta aleatoriamente
+    question = random.choice(questions)
+    
+    # Remove a pergunta da lista de perguntas para não repetir
+    questions.remove(question)
+    
+    # Define o texto da label para exibir a pergunta
+    title2_question["text"] = question
+    
+    # Limpa a entrada de texto
+    input_answer.delete(0, END)
+
+    
+    # Se todas as perguntas já foram respondidas, exibe mensagem final
+    if not questions:
+        title2_question["text"] = "Fim do Quiz!"
+        #input_answer.pack_forget()
+        #button_confirm.pack_forget()
+
+# Chama a função para escolher a primeira pergunta
+next_question()
+
+window.mainloop()
--- Quiz Questions in Python/window.py
+++ Quiz Questions in Python/window.py
@@ -1,6 +1,6 @@
 # Janela Quiz
-
 from tkinter import *
+
 
 #Cores
 cor1 ='#dfe0e8' #gray
@@ -8,6 +8,7 @@ cor2 ='#0dff00' #green
 cor3 ='#3852d1' #blue
 cor4 ='#050505' #black
 
+# Cria uma window
 window = Tk()
 window.title("QUESTÕES SOBRE PYTHON")
 window.geometry('600x350') 
@@ -18,30 +19,36 @@ img = PhotoImage(file="Quiz Questions in Python/imagens/Python_logo.png")
 
 Label.logo = Label(window, image=img, anchor='center').pack()
 
-
+# Cria uma label para exibibição
 title_1 = Label (window, width=50, text= '*****Seja Bem-Vindo ao Quiz Python*****', font=('Arial 15'), fg=cor2, bg='blue', pady=5)
 title_1.place(x=20, y=0)
 
-title_2 = Label (window, width=50, text= "Escolha uma Alternativa  ", font=('Arial 12'), fg='black')
-title_2.place(x=70, y=60)
+title2_pergunta = Label(window, width=50, height=2 , text="", font=('Arial 15'), fg='black')
+title2_pergunta.place(x=20, y=120)
 
-title_3 = Label (window, width=50, height=2 ,text= "Inserir a Pergunta  ", font=('Arial 15'), fg='black')
-title_3.place(x=20, y=120)
+# Cria uma entrada de texto para o usuário digitar a resposta
+input_answer = Entry(window, font=('Arial 15'), bg=cor1, fg=cor4)
+input_answer.place (x=180, y=200)
 
-score = Label (window, width=20, height=2, bg='gray', text= "Your Score = ", font=('Arial 10'), fg='black', anchor='w')
-score.place(x=400, y=300)
+
+# Cria um botão para o usuário confirmar sua resposta
+button_confirm = Button(window, text="Confirmar", font=("Arial", 14))
+button_confirm.place (x=480, y=300)
+
+#score = Label (window, width=20, height=2, bg='gray', text= "Your Score = ", font=('Arial 10'), fg='black', anchor='w')
+#score.place(x=400, y=300)
 
 #Botão
-button_A = Button(window, width=35, height=1, bg=cor1, text="A-) ", relief='raised', fg=cor4, anchor='w')
-button_A.place (x=20, y=200)
+#button_A = Button(window, width=35, height=1, bg=cor1, text="A-) ", relief='raised', fg=cor4, anchor='w')
+#button_A.place (x=20, y=200)
 
-button_B = Button(window, width=35, height=1, bg=cor1, text="B-) ", relief='raised', fg=cor4, anchor='w')
-button_B.place (x=20, y=250)
+#button_B = Button(window, width=35, height=1, bg=cor1, text="B-) ", relief='raised', fg=cor4, anchor='w')
+#button_B.place (x=20, y=250)
 
-button_C = Button(window, width=35, height=1, bg=cor1, text="C-) ", relief='raised', fg=cor4, anchor='w')
-button_C.place (x=300, y=200)
+#button_C = Button(window, width=35, height=1, bg=cor1, text="C-) ", relief='raised', fg=cor4, anchor='w')
+#button_C.place (x=300, y=200)
 
-button_D = Button(window, width=35, height=1, bg=cor1, text="D-) ", relief='raised', fg=cor4, anchor='w')
-button_D.place (x=300, y=250)
+#button_D = Button(window, width=35, height=1, bg=cor1, text="D-) ", relief='raised', fg=cor4, anchor='w')
+#button_D.place (x=300, y=250)
 
 window.mainloop()
